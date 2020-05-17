3a9c5145b4547789a6dfa96f7fe97e6588e1fcc3
Max Han <mwellh84@gmail.com>
2020-05-17 18:14:31 -0400

    update projects section

---
+++ src/components/Projects.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import Image from 'gatsby-image';
+
+const Projects = () => {
+  const {
+    allContentfulProjects: { nodes: projects },
+  } = useStaticQuery(query);
+  console.log('projects are ', projects);
+  return (
+    <div>
+      <h2 className="mb-5">Projects</h2>
+      <section className="projects-section" id="projects">
+        {projects.map((proj, i) => {
+          return (
+            <section className="project">
+              <a href={proj.deployment} className="proj-title-link">
+                <h3>{proj.title}</h3>
+              </a>
+              <a href={proj.deployment}>
+                <Image fluid={proj.gif.fluid} className="screenshot" />
+              </a>
+              <p>{proj.info.info}</p>
+              <p>
+                <i class="fas fa-layer-group"></i>&ensp;{proj.tech.tech}
+              </p>
+              <a href={proj.github}>
+                <p>
+                  <i class="fab fa-github-square"></i>&ensp;View code
+                </p>
+              </a>
+            </section>
+          );
+        })}
+      </section>
+    </div>
+  );
+};
+
+export const query = graphql`
+  {
+    allContentfulProjects {
+      nodes {
+        title
+        info {
+          info
+        }
+        tech {
+          tech
+        }
+        screenshot {
+          fluid {
+            ...GatsbyContentfulFluid_tracedSVG
+          }
+        }
+        gif {
+          fluid {
+            ...GatsbyContentfulFluid_tracedSVG
+          }
+        }
+        github
+        deployment
+      }
+    }
+  }
+`;
+export default Projects;
+
+{
+  /* <div className="d-flex flex-column flex-md-row justify-content-between mb-5">
+<div>
+  <h3 className="mb-0">{proj.title}</h3>
+  <div className="subheading mb-3">{proj.info.info}</div>
+</div>
+<div className="resume-date text-md-right">
+  <Image fluid={proj.gif.fluid} />
+</div>
+</div> */
+}
